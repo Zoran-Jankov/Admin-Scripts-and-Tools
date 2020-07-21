fcce4e6ea18638359be42ba5b2d6fb227545cc6a
Zoran Jankov <zoran.jankov.87@gmail.com>
2020-07-21 13:41:17 +0200

    Initial commit

---
+++ Write-Log.ps1
@@ -0,0 +1,75 @@
+<#
+.SYNOPSIS
+Writes a log entry
+
+.DESCRIPTION
+Creates a log entry with timestamp and message passed thru a parameter $Message or thru pipeline, and saves the log entry to log
+file, to report log file, and writes the same entry to console. $Configuration parameter contains path to Configuration.inf file
+in witch paths to report log and permanent log file are contained, and option to turn on or off whether a report log, permanent
+log and console write should be written. This function can be called to write a log separator, and this entries do not have a
+timestamp. Format of the timestamp is "yyyy.MM.dd. HH:mm:ss:fff" and this function adds " - " after timestamp and before the main
+message.
+
+.PARAMETER Configuration
+Parameter description
+
+.PARAMETER Message
+Parameter description
+
+.PARAMETER LogSeparator
+Parameter description
+
+.EXAMPLE
+An example
+
+.NOTES
+Version:        1.0
+Author:         Zoran Jankov
+Creation Date:  21.07.2020.
+#>
+function Write-Log
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Position=0, Mandatory=$true, ValueFromPipeline=$false)]
+        [String]
+        $Configuration,
+        
+        [Parameter(Position=1, Mandatory=$false, ValueFromPipeline=$true)]
+        [String]
+        $Message,
+
+        [Parameter(Position=2, Mandatory=$false, ValueFromPipeline=$false)]
+        [String]
+        $LogSeparator
+    )
+
+    process
+    {
+        if($LogSeparator -eq $null)
+        {
+            $timestamp = Get-Date -Format "yyyy.MM.dd. HH:mm:ss:fff"
+    	    $logEntry = $timestamp + " - " + $Message
+        }
+        else
+        {
+            $logEntry = $LogSeparator
+        }
+
+        if($Configuration.WRITE_REPORT -eq "true")
+        {
+            Add-content -Path $Configuration.REPORT -Value $logEntry
+        }
+
+        if($Configuration.WRITE_LOG -eq "true")
+        {
+            Add-content -Path $Configuration.LOG -Value $logEntry
+        }
+
+        if($Configuration.WRITE_OUTPUT -eq "true")
+        {
+            Write-Output $logEntry
+        }
+    }
+}
