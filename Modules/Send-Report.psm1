8632b8eb825b06cb3f7b64a7fcbfdcd1792a66ea
Zoran Jankov <zoran.jankov.87@gmail.com>
2020-07-27 09:03:22 +0200

    Update Send-Report.psm1

--- Modules/Send-Report.psm1
+++ Modules/Send-Report.psm1
@@ -3,7 +3,20 @@
 Sends a Report.log file to defined email address
 
 .DESCRIPTION
-This function sends a Report.log file as an attachment to defined email address
+This function sends a Report.log file as an attachment to defined email address. In Configuration hashtable parameter email
+settings are defined.
+
+.PARAMETER Configuration
+Parameter Configuration is a hashtable that contains
+
+.PARAMETER Log
+Parameter description
+
+.EXAMPLE
+An example
+
+.NOTES
+General notes
 #>
 function Send-Report
 {
@@ -13,19 +26,20 @@ function Send-Report
         [hashtable]
         $Configuration,
 
-        [Parameter(Position=0, Mandatory=$true, ValueFromPipelineByPropertyName)]
+        [Parameter(Position=1, Mandatory=$true, ValueFromPipelineByPropertyName)]
         [System.Collection.ArrayList]
         $Log
     )
 
-    if($Configuration.SEND_REPORT -eq "true")
+    if($Configuration.SendReport -eq "true")
     {
         #Creating report log file
         New-Item -Path '.\Report.log' -ItemType File
         $report = '.\Report.log'
-        Add-content -Path $report -Value $Log
+        Add-content -Path $report -Value $Log.
 
-        $body = $Configuration.Body + "`n" + $FinalMessage
+        $body = $Configuration.Body + "`n" + $Log.get($Log.size() - 1)
+
         Send-MailMessage -SmtpServer $Configuration.SmtpServer `
                          -Port $Configuration.Port `
                          -To $Configuration.To `
