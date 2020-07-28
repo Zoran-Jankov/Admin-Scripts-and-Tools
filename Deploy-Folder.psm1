d61b0bf6b5de4673d9005cded0b9cd7b62a4df1a
Zoran Jankov <zoran.jankov.87@gmail.com>
2020-07-28 14:30:35 +0200

    Deploy-Folder

---
+++ Deploy-Folder.psm1
@@ -0,0 +1,52 @@
+<#
+.SYNOPSIS
+Creates transfer folder if it does not already exists
+
+.DESCRIPTION
+This function check if defined transfer folder exists and if not it creates it on remote computer
+
+.PARAMETER Path
+Full path of the folder.
+
+.EXAMPLE
+Deploy-Folder -Path "\\RemoteComputer\D$\Transfer Folder"
+#>
+function Deploy-Folder
+{
+	param
+	(
+		[Parameter(Position = 0, Mandatory = $true)]
+		[String]
+		$Path
+	)
+
+	Import-Module '.\Write-Log.psm1'
+
+    if((Test-Path $Path) -eq $true)
+    {
+		$message = "Successfully accessed " + $Path + " folder"
+		Write-Log -OperationSuccessful "Successful" -Message $message
+		$operationSuccessful = $true
+	}
+	else
+	{
+		$message = "Failed to access " + $Path + " folder - MISSING FOLDER ERROR"
+		Write-Log -OperationSuccessful "Failed" -Message $message
+
+		New-Item -Path $Path -ItemType "Directory"
+
+		if((Test-Path $Path) -eq $true)
+		{
+			$message = "Successfully created " + $Path + " folder"
+			Write-Log -OperationSuccessful "Successful" -Message $message
+			$operationSuccessful = $true
+		}
+		else
+		{
+			$message = "Failed to create " + $Path + " folder"
+			Write-Log  -Message $message
+			$operationSuccessful = $false
+		}
+	}
+	return $operationSuccessful
+}
