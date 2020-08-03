6bf4db7955706949e4f8d0804f1cb82e3bf03cb2
Zoran Jankov <zoran.jankov.87@gmail.com>
2020-08-03 14:46:17 +0200

    Create Get-UsernameFromFullName.psm1

---
+++ Get-UsernameFromFullName.psm1
@@ -0,0 +1,41 @@
+function Get-UsernameFromFullName
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Position = 0, Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+        [string]
+        $FullName,
+
+        [Parameter(Position = 1, Mandatory = $false, ValueFromPipelineByPropertyName = $true)]
+        [ValidateSet('First', 'Last')]
+        [string]
+        $LastNamePosition = 'First'
+    )
+    
+    $usernameComponents = $FullName.ToLower().Split(" ")
+
+    switch ($LastNamePosition)
+    {
+        'First' { $firstName = $usernameComponents.Length -1; $lastName = 0 }
+        'Last'  { $firstName = 0; $lastName = $usernameComponents.Length -1 }
+        Default { $firstName = $usernameComponents.Length -1; $lastName = 0 }
+    }
+
+    $chars = ($usernameComponents.Get($firstName) + "." + $usernameComponents.Get($lastName)).ToCharArray()
+
+    foreach($char in $chars)
+    {
+        switch($char)
+        {
+            'ć' { $segment = "c" }
+            'č' { $segment = "č" }
+            'đ' { $segment = "dj" }
+            'š' { $segment = "s" }
+            'ž' { $segment = "z" }
+            default { $segment = $char }
+        }
+        $Username += $segment
+    }
+    return $Username
+}
