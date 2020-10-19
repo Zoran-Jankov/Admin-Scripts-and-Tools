0791449aeb81f00d6d4bdb11735ed7a8a8131d7a
Zoran Jankov <zoran.jankov.87@gmail.com>
2020-10-19 11:48:32 +0200

    Removed Compare-AzureADUserInfo from Common-PowerShell-Module.psm1

--- Common-PowerShell-Molule.psm1
+++ Common-PowerShell-Molule.psm1
@@ -293,10 +293,7 @@ Short description
 .DESCRIPTION
 Long description
 
-.PARAMETER AzureADUser
-Parameter description
-
-.PARAMETER Employee
+.PARAMETER String
 Parameter description
 
 .EXAMPLE
@@ -305,50 +302,6 @@ An example
 .NOTES
 General notes
 #>
-function Compare-AzureADUserInfo {
-    [CmdletBinding()]
-    param (
-        [Parameter(Position = 0, Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
-        [Microsoft.Open.AzureAD.Model.DirectoryObject]
-        $AzureADUser,
-
-        [Parameter(Position = 1, Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
-        [Object[]]
-        $Employee
-    )
-
-    begin {
-        $UserAttributes = @(
-        'JobTitle',
-        'Department',
-        'StreetAddress',
-        'City',
-        'PostalCode'
-        )
-
-        $Changes = $false
-    }
-
-    process {
-        foreach ($UserAttribute in $UserAttributes) {
-
-            $AzureADUserAttributeValue = $AzureADUser | Select-Object -Property $UserAttribute
-            $UserAttributeValue = $Employee | Select-Object -Property $UserAttribute
-
-            if ($AzureADUserAttributeValue.$UserAttribute -ne $UserAttributeValue.$UserAttribute) {
-                Write-Host -ForegroundColor Green -Object $UserAttribute
-                Write-Host -ForegroundColor Yellow -Object ("Old value: " + $AzureADUserAttributeValue.$UserAttribute)
-                Write-Host -ForegroundColor Cyan -Object ("New value: " + $UserAttributeValue.$UserAttribute + "`n")
-                $Changes = $true
-            }
-        }
-    }
-    
-    end {
-        return $Changes
-    }
-}
-
 function Convert-SerbianToEnglish {
     [CmdletBinding()]
     param (
