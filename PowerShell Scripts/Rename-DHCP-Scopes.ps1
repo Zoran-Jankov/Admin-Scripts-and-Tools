d3aacaffcc9a8be55a79c7411dc3f79652049fcd
Zoran Jankov <zoran.jankov.87@gmail.com>
2021-07-27 22:06:51 +0200

    Create Rename-DHCP-Scopes.ps1

---
+++ PowerShell Scripts/Rename-DHCP-Scopes.ps1
@@ -0,0 +1,7 @@
+﻿$Scopes = Import-Csv -Path "$PSScriptRoot\DHCP-Scopes.csv"
+
+foreach($Scope in $Scopes) {
+    Set-DhcpServerv4Scope -ScopeId $Scope.ScopeId `
+                          -Name $Scope.Name `
+                          -Description $Scope.Description
+}
