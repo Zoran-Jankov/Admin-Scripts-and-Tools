d055d79cd32dd5fc98e01f8765e8aee65c9bead6
Zoran Jankov <zoran.jankov.87@gmail.com>
2021-07-27 22:15:27 +0200

    Create Set-Input-Language-List.ps1

---
+++ PowerShell Scripts/Set-Input-Language-List.ps1
@@ -0,0 +1,5 @@
+﻿$LanguageList = New-WinUserLanguageList -Language "sr-Latn-RS"
+$LanguageList.Add("en-US")
+$LanguageList.Add("sr-Cyrl-RS")
+
+Set-WinUserLanguageList -LanguageList $LanguageList -Confirm:$false -Force
