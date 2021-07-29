3b530e204755f5ea537f3a4c0a1c26a268851f6d
Zoran Jankov <zoran.jankov.87@gmail.com>
2021-07-29 12:36:51 +0200

    Create Add-DNS-Records.ps1

---
+++ PowerShell Scripts/Add-DNS-Records.ps1
@@ -0,0 +1,9 @@
+﻿$Records = Import-Csv -Path "$PSScriptRoot\Records.csv" -Delimiter ';'
+foreach($Record in $Records) {
+    Add-DnsServerResourceRecordA -ComputerName "dns.company.local" `
+                                 -Name $Record.Name `
+                                 -ZoneName "company.local" `
+                                 -IPv4Address $Record.IPv4Address `
+                                 -CreatePtr `
+                                 -Confirm:$false
+}
