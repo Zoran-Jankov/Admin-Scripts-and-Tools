83dae0c629ad2cbd5ec9873bbffcaf4b35db1d30
Zoran Jankov <zoran.jankov.87@gmail.com>
2021-08-03 14:27:16 +0200

    Create Test-Connection.ps1

---
+++ PowerShell Scripts/Test-Connection.ps1
@@ -0,0 +1,10 @@
+﻿$ComputerList = Get-Content -Path ".\ComputerList.txt"
+
+foreach ($Computer in $ComputerList) {
+    if (Test-Connection -ComputerName $Computer -Quiet -Count 1) {
+        Write-Host $Computer -ForegroundColor Green
+    }
+    else {
+        Write-Host $Computer -ForegroundColor Red
+    }
+}
