90c7eee161c6a9ede681610eb5e2a940da3294dd
Zoran Jankov <zoran.jankov.87@gmail.com>
2021-08-03 14:28:04 +0200

    Create Get-Computer-NetBIOS-name.ps1

---
+++ PowerShell Scripts/Get-Computer-NetBIOS-name.ps1
@@ -0,0 +1,6 @@
+﻿$ComputerList = Get-Content -Path ".\ComputerList.txt"
+
+foreach ($Computer in $ComputerList) {
+    $ComputerSystem = Get-WmiObject Win32_ComputerSystem -ComputerName $Computer
+    Write-Host $ComputerSystem.Name -ForegroundColor Green
+}
