2c58d890b44a64024dddfcbac8b2f43ab2a600c8
Zoran Jankov <zoran.jankov.87@gmail.com>
2021-08-06 14:29:28 +0200

    Create Get-Inactive-Computers.ps1

---
+++ PowerShell Scripts/Get-Inactive-Computers.ps1
@@ -0,0 +1,5 @@
+﻿$DaysInactive = 60
+
+$Time = (Get-Date).Adddays(-($DaysInactive))
+
+Get-ADComputer -Filter {LastLogonTimeStamp -lt $Time} -Properties Name, OperatingSystem, LastLogonTimeStamp | Select-Object Name, OperatingSystem | Export-Csv ".\Inactive-Computers.csv"
