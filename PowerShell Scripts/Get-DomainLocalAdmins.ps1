5282cd84af8400251c85ce108e1e653ac201323a
Zoran Jankov <zoran.jankov.87@gmail.com>
2022-01-22 23:04:46 +0100

    Add files via upload

---
+++ PowerShell Scripts/Get-DomainLocalAdmins.ps1
@@ -0,0 +1,15 @@
+﻿$Computers = Get-ADComputer -Filter * -SearchBase "CN=Computers,DC=company,DC=com"
+$LocalAdmins = @()
+
+foreach ($Computer in $Computers) {
+    Invoke-Command -ComputerName $Computer.Name -ScriptBlock {
+        $Admins = Get-LocalGroupMember -Group 'Administrators' | Where-Object -FilterScript {
+            $_.PrincipalSource -eq 'ActiveDirectory'
+            } | ForEach-Object -Process {
+				n
+            $LocalAdmins.Add(($_.Name + " on " + $Computer.Name ))
+        }
+    }
+}
+
+$LocalAdmins | Out-File -FilePath "C:\LocalAdmins.txt"
